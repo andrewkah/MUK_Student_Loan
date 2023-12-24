bc84d443ca402f865c6d0ff448ec2b4a84d5c1b3
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-12-24 21:04:55 +0300

    Add main screen tabs

---
+++ lib/app_screens/listings_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ListingsScreen extends StatelessWidget {
+  const ListingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Text("Listings"),
+    );
+  }
+}
---
+++ lib/app_screens/transactions_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TransactionsScreen extends StatelessWidget {
+  const TransactionsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Text("Transactions"),
+    );
+  }
+}
