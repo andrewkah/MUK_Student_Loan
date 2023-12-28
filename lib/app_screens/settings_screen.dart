a9da7b6ba8e26e521bb5b5a08d82527b93d8a9a5
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-12-28 22:08:39 +0300

    Updated Settings screen

--- lib/app_screens/settings_screen.dart
+++ lib/app_screens/settings_screen.dart
@@ -1,12 +1,223 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool balanceSwitch = false;
+  bool fingerprintSwitch = false;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Text("Settings"),
-    );
+        child: Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(24.0, 20.0, 24.0, 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Notifications",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Notification Settings",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_forward_ios_rounded),
+                iconSize: 25,
+              ),
+              SizedBox(
+                width: 10,
+              )
+            ],
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 2,
+            indent: 3,
+            endIndent: 3,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Column(
+                children: [
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Privacy & Settings",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Hide Balance",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Hide your balance on the Home screen",
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Switch(
+                  value: balanceSwitch,
+                  activeColor: Colors.primaryGreen,
+                  onChanged: (bool value) {
+                    // This is called when the user toggles the switch.
+                    setState(() {
+                      balanceSwitch = value;
+                    });
+                  }),
+              const SizedBox(
+                width: 8,
+              ),
+            ],
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 2,
+            indent: 3,
+            endIndent: 3,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Column(
+                children: [
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Enable fingerprint",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Hide your balance on the Home screen",
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Switch(
+                  value: fingerprintSwitch,
+                  activeColor: Colors.primaryGreen,
+                  onChanged: (bool value) {
+                    setState(() {
+                      fingerprintSwitch = value;
+                    });
+                  }),
+              const SizedBox(
+                width: 8,
+              ),
+            ],
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 2,
+            indent: 3,
+            endIndent: 3,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+            child: Column(
+              children: [
+                Text(
+                  "Other",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "FAQs",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 2,
+            indent: 3,
+            endIndent: 3,
+          ),
+          const Spacer(),
+          Align(
+            alignment: Alignment.center,
+            child: MaterialButton(
+              onPressed: () {},
+              child: const Text(
+                "SIGN OUT",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          )
+        ],
+      ),
+    ));
   }
 }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-
   final String title;
   final double? titleSize;
   final double paddingHorizontal;
   final double paddingVertical;
   final double elevation;
   final double borderRadius;
-  const CustomButton({super.key, required this.title, required this.paddingHorizontal, required this.paddingVertical, required this.borderRadius, required this.elevation, this.titleSize});
+  const CustomButton(
+      {super.key,
+      required this.title,
+      required this.paddingHorizontal,
+      required this.paddingVertical,
+      required this.borderRadius,
+      required this.elevation,
+      this.titleSize});
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +23,17 @@ class CustomButton extends StatelessWidget {
       borderRadius: BorderRadius.circular(borderRadius),
       color: Colors.primaryGreen,
       child: MaterialButton(
-        padding: EdgeInsets.symmetric(vertical: paddingVertical, horizontal: paddingHorizontal),
-        onPressed: () {
-
-        },
-        child: Text(title, textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.white, fontSize: titleSize, fontWeight: FontWeight.bold),),
+        padding: EdgeInsets.symmetric(
+            vertical: paddingVertical, horizontal: paddingHorizontal),
+        onPressed: () {},
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: titleSize,
+              fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
