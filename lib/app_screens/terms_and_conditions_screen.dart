386e9ba79cd589f618ac49ab58634798487ed109
Kalema <kalema3502@gmail.com>
2024-01-07 22:32:37 +0300

    Translation Done

--- lib/app_screens/terms_and_conditions_screen.dart
+++ lib/app_screens/terms_and_conditions_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:core';
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:google_translate/google_translate.dart';
+import 'package:mak_scholar1/app_screens/progress.dart';
 import 'package:mak_scholar1/widgets/custom_appbar.dart';
 import 'package:mak_scholar1/widgets/custom_button.dart';
 
@@ -16,14 +20,118 @@ class _TermsConditionsScreenState extends State<TermsConditionsScreen> {
   final TextStyle? unSelectedStyle = const TextStyle(fontSize: 15,);
   final TextStyle? selectedStyle = const TextStyle(fontSize: 15, color: Colors.grey);
 
+  String textOneOriginal = "Loan Agreement Terms and Conditions";
+  String textOneTranslated = "";
+  String textTwoOriginal = "COMMENCEMENT";
+  String textTwoTranslated = "";
+  String textThreeOriginal = "These terms form an agreement between you and us, so please make sure that you understand all of them. You agree that you will only use this mobile application in line with these terms and any additional terms mentioned below that may apply, including any terms and conditions incorporated by reference to applicable laws, rules and regulations. Important clauses, which may limit out responsibility or invlove some risk for you will be in bold or italics. You must pay special attention to these clauses.";
+  String textThreeTranslated = "";
+  String textFourOriginal = "This mobile application is provided by Makerere Univeristy to enable financially unstable students access student loan services offered by Makerere University in collaboration with the Higher Education Students Funding Board. The terms and conditions apply when using the mobile application and the laws of jurisdiction from or in which those services are provided to you apply accordingly ";
+  String textFourTranslated = "";
+  String textFiveOriginal = "Definitions";
+  String textFiveTranslated = "";
+  String textSevenOriginal = "We have defined some words for consistency. These words will begin with a capital letter, where indicated. Singular words include the plural and the other was around";
+  String textSevenTranslated = "";
+  String textEightOriginal = "Access Codes: ";
+  String textEightTranslated = "";
+  String textSixOriginal = " Any of your secret numbers used to access our services, including your personal Identification Number (PIN) or one-time PIN (OTP), passwords, usernames, or email addresses.";
+  String textSixTranslated = "";
+  String textNineOriginal = "Account: ";
+  String textNineTranslated = "";
+  String textTenOriginal = " Any account or portfolio opened at your request by us in your name, or for which you are responsible.";
+  String textTenTranslated = "";
+  String textElevenOriginal = "App Store: ";
+  String textElevenTranslated = "";
+  String textTwelveOriginal = "Your Devices's Application store provided by, for example Apple, Blackberry or Android as is applicable to you, from which you download the application.";
+  String textTwelveTranslated = "";
+  String textThirteenOriginal = "An internet service provider, which is an organisation that provides access to the internet.";
+  String textThirteenTranslated = "";
+  String textFourteenOriginal = "A One-Time Pin which you will receive to validate your transaction and which is valid for Transaction only.";
+  String textFourteenTranslated = "";
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: CustomAppBar(
         leadingIcon: Icons.arrow_back_ios_new_rounded,
+        trailingIcon: Icons.translate_outlined,
         title: "T&C",
         onPressed: () {
           Navigator.of(context).pop(true);
+        },
+        onTrailingPressed: () async {
+          // Navigator.of(context).push(MaterialPageRoute(builder: (_) => const AutoBlurredCircularProgress()));
+          await textOneOriginal.translate().then((value) {
+            setState(() {
+              textOneTranslated = value;
+            });
+          });
+          await textTwoOriginal.translate().then((value) {
+            setState(() {
+              textTwoTranslated = value;
+            });
+          });
+          await textThreeOriginal.translate().then((value) {
+            setState(() {
+              textThreeTranslated = value;
+            });
+          });
+          await textFourOriginal.translate().then((value) {
+            setState(() {
+              textFourTranslated = value;
+            });
+          });
+          await textFiveOriginal.translate().then((value) {
+            setState(() {
+              textFiveTranslated = value;
+            });
+          });
+          await textSevenOriginal.translate().then((value) {
+            setState(() {
+              textSevenTranslated = value;
+            });
+          });
+          await textSixOriginal.translate().then((value) {
+            setState(() {
+              textSixTranslated = value;
+            });
+          });
+          await textEightOriginal.translate().then((value) {
+            setState(() {
+              textEightTranslated = value;
+            });
+          });
+          await textNineOriginal.translate().then((value) {
+            setState(() {
+              textNineTranslated = value;
+            });
+          });
+          await textTenOriginal.translate().then((value) {
+            setState(() {
+              textTenTranslated = value;
+            });
+          });
+          await textElevenOriginal.translate().then((value) {
+            setState(() {
+              textElevenTranslated = value;
+            });
+          });
+          await textTwelveOriginal.translate().then((value) {
+            setState(() {
+              textTwelveTranslated = value;
+            });
+          });
+          await textThirteenOriginal.translate().then((value) {
+            setState(() {
+              textThirteenTranslated = value;
+            });
+          });
+          await textFourteenOriginal.translate().then((value) {
+            setState(() {
+              textFourteenTranslated = value;
+            });
+          });
         },
       ),
       body: Stack(
@@ -34,14 +142,14 @@ class _TermsConditionsScreenState extends State<TermsConditionsScreen> {
                 Container(
                   padding: const EdgeInsets.symmetric(
                       vertical: 15.0, horizontal: 24.0),
-                  child: const Column(
+                  child: Column(
                     children: <Widget>[
                       Text(
-                        "Loan Agreement Terms and Conditions",
-                        style: TextStyle(
+                       textOneTranslated.isNotEmpty ? textOneTranslated : textOneOriginal,
+                        style: const TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 18),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 20,
                       ),
                       // Text(
@@ -53,33 +161,34 @@ class _TermsConditionsScreenState extends State<TermsConditionsScreen> {
                         children: [
                           Text.rich(
                             TextSpan(
-                              style: TextStyle(
+                              style: const TextStyle(
                                 fontSize: 16,
                               ),
                               children: <TextSpan>[
-                                TextSpan(text: "1. "),
+                                const TextSpan(text: "1. "),
                                 TextSpan(
-                                  text: "INTRODUCTION",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  text: textTwoTranslated.isNotEmpty ? textTwoTranslated.toUpperCase() : textTwoOriginal,
+                                  style: const TextStyle(fontWeight: FontWeight.bold),
                                 ),
                               ],
                             ),
                           ),
                         ],
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 10,
                       ),
                       Text(
-                          "These terms form an agreement between you and us, so please make sure that you understand all of them. You agree that you will only use this mobile application in line with these terms and any additional terms mentioned below that may apply, including any terms and conditions incorporated by reference to applicable laws, rules and regulations. Important clauses, which may limit out responsibility or invlove some risk for you will be in bold or italics. You must pay special attention to these clauses.",
-                        style: TextStyle(fontSize: 15),),
-                      SizedBox(
+                        textThreeTranslated.isNotEmpty ? textThreeTranslated : textThreeOriginal,
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                      const SizedBox(
                         height: 5,
                       ),
                       Text(
-                          "This mobile application is provided by Makerere Univeristy to enable financially unstable students access student loan services offered by Makerere University in collaboration with the Higher Education Students Funding Board. The terms and conditions apply when using the mobile application and the laws of jurisdiction from or in which those services are provided to you apply accordingly ",
-                        style: TextStyle(fontSize: 15),),
-                      SizedBox(
+                        textFourTranslated.isNotEmpty ? textFourTranslated : textFourOriginal,
+                        style: const TextStyle(fontSize: 15),),
+                      const SizedBox(
                         height: 10,
                       ),
                       Row(
@@ -87,126 +196,138 @@ class _TermsConditionsScreenState extends State<TermsConditionsScreen> {
                         children: [
                           Text.rich(
                             TextSpan(
-                              style: TextStyle(
+                              style: const TextStyle(
                                 fontSize: 16,
                               ),
                               children: <TextSpan>[
-                                TextSpan(text: "2. "),
+                                const TextSpan(text: "2. "),
                                 TextSpan(
-                                  text: "DEFINITIONS",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  text: textFiveTranslated.isNotEmpty ? textFiveTranslated.toUpperCase() : textFiveOriginal,
+                                  style: const TextStyle(fontWeight: FontWeight.bold),
                                 ),
                               ],
                             ),
                           ),
                         ],
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 5,
                       ),
                       Text(
-                          "We have defined some words for consistency. These words will begin with a capital letter, where indicated. Singular words include the plural and the other was around",
-                        style: TextStyle(fontSize: 15),
+                          textSevenTranslated.isNotEmpty ? textSevenTranslated : textSevenOriginal,
+                        style: const TextStyle(fontSize: 15),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 5,
                       ),
                       Text.rich(
                         TextSpan(
-                            style: TextStyle(
+                            style: const TextStyle(
                               fontSize: 16,
                             ),
                             children: <TextSpan>[
-                              TextSpan(text: "2.1 "),
+                              const TextSpan(text: "2.1 ",
+                                style: TextStyle(fontSize: 15),
+                              ),
                               TextSpan(
-                                  text: "Access Codes: ",
+                                  text: textEightTranslated.isNotEmpty ? textEightTranslated : textEightOriginal,
                                   style:
-                                      TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                                      const TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
                               TextSpan(
                                   text:
-                                      "Any of your secret numbers used to access our services, including your personal Identification Number (PIN) or one-time PIN (OTP), passwords, usernames, or email addresses.",
-                                style: TextStyle(fontSize: 15),)
+                                    textSixTranslated.isNotEmpty ? textSixTranslated : textSixOriginal,
+                                style: const TextStyle(fontSize: 15),)
                             ]),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 5,
                       ),
                       Text.rich(
                         TextSpan(
-                            style: TextStyle(
+                            style: const TextStyle(
                               fontSize: 16,
                             ),
                             children: <TextSpan>[
-                              TextSpan(text: "2.2 "),
+                              const TextSpan(text: "2.2 ",
+                                style: TextStyle(fontSize: 15),
+                              ),
                               TextSpan(
-                                  text: "Account: ",
+                                  text: textNineTranslated.isNotEmpty ? textNineTranslated : textNineOriginal,
                                   style:
-                                      TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                                      const TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
                               TextSpan(
                                   text:
-                                      "Any account or portfolio opened at your request by us in your name, or for which you are responsible.",
-                                style: TextStyle(fontSize: 15),)
+                                  textTenTranslated.isNotEmpty ? textTenTranslated : textTenOriginal,
+                                style: const TextStyle(fontSize: 15),)
                             ]),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 5,
                       ),
                       Text.rich(
                         TextSpan(
-                            style: TextStyle(
+                            style: const TextStyle(
                               fontSize: 16,
                             ),
                             children: <TextSpan>[
-                              TextSpan(text: "2.3 "),
+                              const TextSpan(text: "2.3 ",
+                                style: TextStyle(fontSize: 15),
+                              ),
                               TextSpan(
-                                  text: "App Store: ",
+                                  text: textElevenTranslated.isNotEmpty ? textElevenTranslated : textElevenOriginal,
                                   style:
-                                  TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                                  const TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
                               TextSpan(
-                                  text:
-                                  "Your Devices's Application store provided by, for example Apple, Blackberry or Android as is applicable to you, from which you download the app",
-                                style: TextStyle(fontSize: 15),)
+                                  text: textTwelveTranslated.isNotEmpty ? textTwelveTranslated : textTwelveOriginal,
+                                style: const TextStyle(fontSize: 15),)
                             ]),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 5,
                       ),
                       Text.rich(
                         TextSpan(
-                            style: TextStyle(
+                            style: const TextStyle(
                               fontSize: 16,
                             ),
                             children: <TextSpan>[
-                              TextSpan(text: "2.3 "),
-                              TextSpan(
+                              const TextSpan(text: "2.4 ",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                              const TextSpan(
                                   text: "ISP: ",
                                   style:
                                   TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
                               TextSpan(
                                   text:
-                                  "An internet service provider, which is an organisation that provides access to the internet.",
-                                style: TextStyle(fontSize: 15),)
+                                  textThirteenTranslated.isNotEmpty ? textThirteenTranslated : textThirteenOriginal,
+                                style: const TextStyle(fontSize: 15),)
                             ]),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 5,
                       ),
                       Text.rich(
                         TextSpan(
-                            style: TextStyle(
+                            style: const TextStyle(
                               fontSize: 16,
                             ),
                             children: <TextSpan>[
-                              TextSpan(text: "2.3 "),
-                              TextSpan(
+                              const TextSpan(text: "2.5 ",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                              const TextSpan(
                                   text: "OTP: ",
                                   style:
                                   TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
                               TextSpan(
                                   text:
-                                  "A One-Time Pin which you will receive to validate your transaction and which is valid for Transaction only.",
-                                style: TextStyle(fontSize: 15),)
+                                  textFourteenTranslated.isNotEmpty ? textFourteenTranslated : textFourteenOriginal,
+                                style: const TextStyle(fontSize: 15),)
                             ]),
+                      ),
+                      const SizedBox(
+                        height: 200,
                       ),
                     ],
                   ),
